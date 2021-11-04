8468685aaa38622867f562fb3c56574fad2a2cea
GiseleCavalli <giselecavalli28@hotmail.com>
2021-11-03 21:59:59 -0300

    Layout tela de ranking

--- src/pages/ranking/index.js
+++ src/pages/ranking/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import gomi from "../../assets/telaDescarte/gomi.png"
 import gomiNome from "../../assets/telaDescarte/gomiNome.png"
+import ranking from "../../assets/ranking/ranking.png"
 import Menu from './headersRanking.js';
+import './ranking.css';
 class App extends Component {
 
   constructor(props) {
@@ -17,44 +19,60 @@ class App extends Component {
   render() {
     return (
       <div>
-
-        <div className="body">
-
-          <div className="bcg-cabecalho">
-
-            <img className="imgGomi" src={gomi}></img>
-            <img className="imgGomiNome" src={gomiNome}></img>
-            <Menu />
-          </div>
+        
+          <div className="body">
+            <div className="bcg-cabecalho">
+              <img className="imgGomi" src={gomi}></img>
+              <img className="imgGomiNome" src={gomiNome}></img>
+              <Menu />
+            </div>
+            <div className="img-ranking">
+              
+          {/* Tabela do Ranking */}
           
-          <div class="container">
-            <div class="row align-items-start">
-              <div class="col">
-                Posição
-                <p>1º</p>
-                <p>2º</p>
-                <p>3º</p>
-                <p>4º</p>
-                <p>5º</p>
-              </div>
-              <div class="col">
+            <div className="table-ranking">
+              
+              <div class="container">
+                  <div class="row align-items-start">
+                    <div class="col">
+                      <p className="title-column-ranking">Posição</p>
+                      <p className="first-position-ranking">1º</p>
+                      <p className="second-position-ranking">2º</p>
+                      <p className="third-position-ranking">3º</p>
+                      <p>4º</p>
+                      <p>6º</p>
+                      <p>7º</p>
+                      <p>8º</p>
+                      <p>9º</p>
+                      <p>10º</p>
+                    </div>
+                  
+                  <div class="col">
+                    <p className="title-column-ranking">Nome</p>
+                    <p className="first-position-ranking">Gisele</p>
+                    <p className="second-position-ranking">Gabriela</p>
+                    <p className="third-position-ranking">Cavalli</p>
+                    <p>Geancarlo</p>
+                    <p>Dahle</p>
+                    <p>Dahle</p>
+                    <p>Dahle</p>
+                    <p>Dahle</p>
+                    <p>Dahle</p>
+                  </div>
 
-                Nome
-                <p>Gisele</p>
-                <p>Gabriela</p>
-                <p>Cavalli</p>
-                <p>Geancarlo</p>
-                <p>Dahle</p>
-
-              </div>
-
-              <div class="col">
-                Pontos
-                <p>100</p>
-                <p>100</p>
-                <p>100</p>
-                <p>100</p>
-                <p>100</p>
+                  <div class="col">
+                    <p className="title-column-ranking">Pontos</p>
+                    <p className="first-position-ranking">100</p>
+                    <p className="second-position-ranking">100</p>
+                    <p className="third-position-ranking">100</p>
+                    <p>100</p>
+                    <p>100</p>
+                    <p>100</p>
+                    <p>100</p>
+                    <p>100</p>
+                    <p>100</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
